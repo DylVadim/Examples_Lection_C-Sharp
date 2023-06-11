ae6624943d67efcde1d529af491c52d7a7033a9a
Vadim Dyl <dyl.vadim.a@gmail.com>
2023-06-11 18:44:41 +0400

    Example005_ConditionIfElse add

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Маша")
+{
+    Console.WriteLine("Ура, Это же МАШ!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
