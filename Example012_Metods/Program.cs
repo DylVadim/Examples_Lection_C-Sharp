8c53a12dcbd9b54593f3e4a734855cf35f473e9d
Vadim <dyl.vadim.a@gmail.com>
2023-06-23 20:48:47 +0400

    DopInfo012_metod_For

--- Example012_Metods/Program.cs
+++ Example012_Metods/Program.cs
@@ -36,6 +36,23 @@ int year = Method3();
 //Console.WriteLine(year);
 
 // Вид 4 Что-то принимают и что-то возвращают
+/*
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = string.Empty;
+
+     while (i < count)
+     {
+        result = result + text;
+        i++;
+     }
+     return result;
+}
+
+string res = Method4(10, "qwerty ");
+Console.WriteLine(res);
+*/
 string Method4(int count, string text)
 {
     int i = 0;
