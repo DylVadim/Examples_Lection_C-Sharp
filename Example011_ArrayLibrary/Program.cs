c0ccb0590d7979e92a3ebe9b2bf32ecf6f78f991
Vadim <dyl.vadim.a@gmail.com>
2023-06-23 19:24:08 +0400

    Final011_ArryLibary
    Решения поиска индекса массива в случайно сгенерированном массиве

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // означает что элемента нет в массиве
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break; //Если убрать оператор break будет искатся последний элемент из массива
+        }
+        index++;
+    }
+    return position;
+}
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
