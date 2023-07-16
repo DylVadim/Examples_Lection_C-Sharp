335d90e66c37bcd4173cb546bbfdaab7e2f308c3
Vadim <54818214+DylVadim@users.noreply.github.com>
2023-07-16 10:06:11 +0400

    Задача 012 обновлена

--- Example012_Metods/Program.cs
+++ Example012_Metods/Program.cs
@@ -53,6 +53,7 @@ string Method4(int count, string text)
 string res = Method4(10, "qwerty ");
 Console.WriteLine(res);
 */
+/*
 string Method4(int count, string text)
 {
     int i = 0;
@@ -68,3 +69,29 @@ string Method4(int count, string text)
 
 string res = Method4(10, "qwerty ");
 Console.WriteLine(res);
+*/
+
+// Цикл for
+/*
+string Metod4(int count, string text)
+{
+    string result = String.Empty;
+        for (int i = 0; i<count; i++)
+        {
+            result = result + text;
+        }
+        return result;
+}
+string res = Metod4(10, "asdf ");
+Console.WriteLine(res);
+*/
+
+// Вывод таблицы умножения
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i}*{j} = {i*j}");
+    }
+    Console.WriteLine();
+}
