85765ccf3d405ea4dbaf5b55ecf517915d93fc69
Vadim <54818214+DylVadim@users.noreply.github.com>
2023-07-16 10:47:05 +0400

    Задача 012 обновлена - работа с текстом

--- Example012_Metods/Program.cs
+++ Example012_Metods/Program.cs
@@ -87,6 +87,7 @@ Console.WriteLine(res);
 */
 
 // Вывод таблицы умножения
+/*
 for (int i = 2; i <= 10; i++)
 {
     for (int j = 2; j <= 10; j++)
@@ -95,3 +96,39 @@ for (int i = 2; i <= 10; i++)
     }
     Console.WriteLine();
 }
+*/
+
+// Работа с текстом
+/* Дан текст. В тексте нужно все пробелы заменить чёрточками,
+маленькие буквы "к" заменить большими "К",
+а большие "С" маленькими "с".
+
+Ясна ли задача? */
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+string Replase(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+string newText = Replase(text, ' ', '_');
+
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replase(newText,'к','К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replase(newText,'с','С');
+Console.WriteLine(newText);
+Console.WriteLine();
